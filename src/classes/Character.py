1a800e17fbd195e661709782acc0c13a2fbcdb1c
ds235410 <ds235410@28PSQ22.AC.STCHAS.EDU>
2018-04-11 20:34:56 -0500

    initial commit

---
+++ src/classes/Character.py
@@ -0,0 +1,15 @@
+'''
+Created on Apr 11, 2018
+
+@author:   Daniel.Swan
+@email:    ds235410@my.stchas.edu
+'''
+class Character():
+    
+    def __init__(self):
+        self.characterName = ""
+        self.characterClass = ""
+        self.strength = 0
+        self.quickness = 0
+        self.armor = 0
+        self.health = 0
