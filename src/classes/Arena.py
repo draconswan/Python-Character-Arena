20734ce959d658253539de5c89866b886f59f446
Daniel Swan <draconswan@gmail.com>
2018-04-12 19:09:48 -0500

    adding start of arena classˆ

---
+++ src/classes/Arena.py
@@ -0,0 +1,15 @@
+"""
+Created on 4/12/18
+
+@author:   Daniel Swan
+@email:    ds235410@my.stchas.edu
+"""
+
+
+class Arena:
+
+    def __init__(self):
+        self.player = None
+        self.opponent = None
+        self.currentRound = 0
+        self.maxRounds = 10
