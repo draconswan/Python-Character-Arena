d486f66709a06ebdefcb6dd1f40b4ed68de0e4b8
ds235410 <ds235410@28PSQ22.AC.STCHAS.EDU>
2018-04-25 18:34:49 -0500

    adding base methods

--- src/classes/Arena.py
+++ src/classes/Arena.py
@@ -13,3 +13,40 @@ class Arena:
         self.opponent = None
         self.currentRound = 0
         self.maxRounds = 10
+    
+    def setPlayer(self, player):
+        self.player = player
+    
+    def setOpponent(self, opponent):
+        self.opponent = opponent
+        
+    def startBattle(self):
+        for index in range(1, self.maxRounds + 1):
+            print("Round %d, Fight!" % index)
+            self.currentRound = index
+            shouldContinue = self.battleRound()
+            if shouldContinue:
+                pass
+            
+    def battleRound(self):
+        battleOver = self.makeAttack(self.player, self.opponent)
+        if not battleOver:
+            return self.makeAttack(self.opponent, self.player)
+        else:
+            return battleOver
+    
+    def makeAttack(self, member1, member2):
+        attackRoll = member1.weaponAttackRoll()
+        if attackRoll >= member2.armor:
+            print("%s's attack hits!")
+            attackDamage = member1.weaponAttackDamage()
+            stillAlive = member2.takeDamage(attackDamage)
+            print("%s takes %d damage." % (member2.name, attackDamage))
+            if not stillAlive:
+                print("%s is defeated!" % (member2.name))
+                return True
+            else:
+                return False
+        else:
+            print("%s's attack misses")
+            return False
